130c42d2dea67bf0a24b7bb57277972c2845fe8a
Marc <kristoffertumarao@gmail.com>
2021-05-21 19:29:36 +0800

    updated usersignin controller

--- UserControllers/userSignin.js
+++ UserControllers/userSignin.js
@@ -1,19 +1,40 @@
 const handleUserSignin = (req, res, db, bcrypt) => {
-	const { email, password } = req.body;
-	if (!email || !password) {
+	const { signInEmail, signInPassword } = req.body;
+	if (!signInEmail || !signInPassword) {
 		return res.status(400).json('incorrect form submission');
 	}
-	db.select('email', 'hash').from('login')
-	.where('email', '=', req.body.email)
+	db.select('user_id','user_email', 'hash').from('user_login')
+	.where('user_email', '=', req.body.signInEmail)
 		.then(data => {
-			const isValid = bcrypt.compareSync(password, data[0].hash);
+			const isValid = bcrypt.compareSync(signInPassword, data[0].hash);
 			if (isValid) {
-				return db.select('*').from('users')
-					.where('email', '=', email)
-					.then(user => {
-						res.json(user[0])
-					})
-					.catch(err => res.status(400).json('unable to get user'))
+				db.select('*').from('requests').where('user_id', '=', data[0].user_id)
+				.then(row => {
+					if (row.length === 0){
+						return db.select('*').from('users').where('user_id',  '=', data[0].user_id)
+					}
+					return db('users').join('requests', data[0].user_id, '=', 'requests.user_id')
+					.orderBy('requests.req_id', 'desc')
+					.select('*')
+				})
+				.then(user => {
+					res.json(user[0])
+				})
+				.catch(err => res.status(400).json('unable to get user'))
+				// db('users').join('requests', data[0].user_id, '=', 'requests.user_id')
+				// .orderBy('requests.req_id', 'desc')
+				// .select('*')
+				// .then(user => {
+				// 	res.json(user[0])
+				// })
+				// .catch(err => res.status(400).json('unable to get user'))
+
+				// db.select('*').from('users')
+				// .where('user_email', '=', signInEmail)
+				// .then(user => {
+				// 	res.json(user[0])
+				// })
+				// .catch(err => res.status(400).json('unable to get user'))
 			} else {
 				res.status(400).json("wrong credentials")
 			}
