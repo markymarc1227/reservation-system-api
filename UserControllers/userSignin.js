53ac3b2368186aa27497058f56257261fc759990
Marc <kristoffertumarao@gmail.com>
2021-05-22 09:02:04 +0800

    updated usersignin controller

--- UserControllers/userSignin.js
+++ UserControllers/userSignin.js
@@ -3,7 +3,7 @@ const handleUserSignin = (req, res, db, bcrypt) => {
 	if (!signInEmail || !signInPassword) {
 		return res.status(400).json('incorrect form submission');
 	}
-	db.select('user_id','user_email', 'hash').from('user_login')
+	db.select('user_id', 'user_email', 'hash').from('user_login')
 	.where('user_email', '=', req.body.signInEmail)
 		.then(data => {
 			const isValid = bcrypt.compareSync(signInPassword, data[0].hash);
@@ -12,29 +12,15 @@ const handleUserSignin = (req, res, db, bcrypt) => {
 				.then(row => {
 					if (row.length === 0){
 						return db.select('*').from('users').where('user_id',  '=', data[0].user_id)
-					}
-					return db('users').join('requests', data[0].user_id, '=', 'requests.user_id')
+					} 
+					return db('users').join('requests', 'users.user_id', 'requests.user_id')
+					.select('*').where('requests.user_id', '=', data[0].user_id)
 					.orderBy('requests.req_id', 'desc')
-					.select('*')
 				})
 				.then(user => {
 					res.json(user[0])
 				})
 				.catch(err => res.status(400).json('unable to get user'))
-				// db('users').join('requests', data[0].user_id, '=', 'requests.user_id')
-				// .orderBy('requests.req_id', 'desc')
-				// .select('*')
-				// .then(user => {
-				// 	res.json(user[0])
-				// })
-				// .catch(err => res.status(400).json('unable to get user'))
-
-				// db.select('*').from('users')
-				// .where('user_email', '=', signInEmail)
-				// .then(user => {
-				// 	res.json(user[0])
-				// })
-				// .catch(err => res.status(400).json('unable to get user'))
 			} else {
 				res.status(400).json("wrong credentials")
 			}
